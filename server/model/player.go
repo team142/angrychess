143aec046c3a0661ac91ab77f1e5cd7fba70f1fa
Just1689 <Just1689@gmail.com>
2019-01-01 18:34:26 +0200

    Bug fix
    Signed-off-by: Just1689 <Just1689@gmail.com>

--- server/model/player.go
+++ server/model/player.go
@@ -4,11 +4,10 @@ type Player struct {
 	Profile *Profile
 	Color   bool
 	Team    int
-	Board   *Board
 	Pieces  []*Piece
 }
 
-func (p *Player) GetPieceByID(id string) (piece Piece, found bool) {
+func (p *Player) GetPieceByID(id string) (piece *Piece, found bool) {
 	for _, p := range p.Pieces {
 		if p.ID == id {
 			piece, found = p, true
