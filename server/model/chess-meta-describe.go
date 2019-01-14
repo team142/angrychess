b8ce896d095bc19a3a9f998372db37bd3671cc98
Just1689 <Just1689@gmail.com>
2019-01-14 19:52:51 +0200

    Describe a move
    Signed-off-by: Just1689 <Just1689@gmail.com>

---
+++ server/model/chess-meta-describe.go
@@ -0,0 +1,26 @@
+package model
+
+import "github.com/team142/chessfor4/util"
+
+type MoveDescription struct {
+	XDiff        int
+	YDiff        int
+	Down         bool //Y is decreasing
+	Diagonal     bool
+	BeingPlaced  bool
+	MovingBoards bool
+	PawnOnSpawn  bool
+}
+
+func CalcMoveDescription(game *Game, player *Player, piece *Piece, move *MessageMove) *MoveDescription {
+	result := &MoveDescription{}
+
+	result.YDiff = util.Abs(piece.Y - move.ToY)
+	result.XDiff = util.Abs(piece.X - move.ToX)
+	result.Down = piece.Y > move.ToY
+	result.BeingPlaced = piece.Cache && !move.Cache
+	result.MovingBoards = piece.Board != move.Board
+	result.PawnOnSpawn = player.Team == 1 && piece.Y == 7 || player.Team == 2 && piece.Y == 2
+
+	return result
+}
