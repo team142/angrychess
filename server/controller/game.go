cff61b949ddd7efda73390a9fee6e3420814d9bb
Justin Tamblyn <just1689@gmail.com>
2019-02-08 13:56:12 +0200

    Pointers not values

--- server/controller/game.go
+++ server/controller/game.go
@@ -37,7 +37,7 @@ func startGame(game *model.Game) {
 //ShareState tells all players what is going on
 func shareState(game *model.Game) {
 	reply := model.CreateMessageShareState(game)
-	b, err := json.Marshal(&reply)
+	b, err := json.Marshal(reply)
 	if err != nil {
 		log.Println(err)
 		return
